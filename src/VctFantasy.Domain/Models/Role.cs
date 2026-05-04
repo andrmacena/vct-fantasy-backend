02a536182bcec9ac0eb3e8c41953538ffd525a29
André Macena <andremacena97@gmail.com>
2026-05-04 13:35:54 -0300

    refactor: change name class Roles to Role

--- src/VctFantasy.Domain/Models/Role.cs
+++ src/VctFantasy.Domain/Models/Role.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace VctFantasy.Domain.Models
 {
-    public class Roles
+    public class Role
     {
         public int Id { get; set; }
         public string Name { get; set; }
