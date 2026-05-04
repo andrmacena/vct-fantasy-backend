c3918c1cef2c5e7092cd702cdfc55f3cc01e48ea
André Macena <andremacena97@gmail.com>
2026-05-04 13:36:16 -0300

    feat: add UseCase to registerController

---
+++ src/VctFantasy.Domain/UseCases/RegisterUserUseCase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VctFantasy.Domain.Context;
+using VctFantasy.Domain.Models;
+
+namespace VctFantasy.Domain.UseCases
+{
+    public class RegisterUserUseCase
+    {
+        private readonly VctFantasyContext _context;
+        public RegisterUserUseCase(VctFantasyContext context) { _context = context; }
+
+        public string RegisterUser(User user)
+        {
+            try
+            {
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return "User registered successfully";
+            }
+            catch (SqlException ex)
+            {
+                return ex.Message;
+            }
+            
+        }
+    }
+}
---
+++ src/VctFantasy/Controllers/RegisterController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VctFantasy.Domain.Context;
+using VctFantasy.Domain.Models;
+using VctFantasy.Domain.UseCases;
+
+namespace VctFantasy.Controllers
+{
+    [ApiController]
+    [Route("v1/register")]
+    public class RegisterController : Controller
+    {
+        private readonly RegisterUserUseCase _registerUserUseCase;
+        public RegisterController(RegisterUserUseCase registerUserUseCase)
+        {
+            _registerUserUseCase = registerUserUseCase;
+        }
+
+        [HttpPost]
+        [Route("users")]
+        public IActionResult RegisterUser([FromBody] User user)
+        {
+            
+            _registerUserUseCase.RegisterUser(user);
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        [Route("teams")]
+        [Authorize(Roles = "user")]
+        public IActionResult AllUsers()
+        {
+
+            return Ok(new User() { Id = 1, Email = "teste@gmail", CreatedAt = DateTime.UtcNow, Roles = new List<Role> { new Role { Id = 1, Name = "admin" } } });
+        }
+    }
+}
