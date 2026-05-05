e48a96a915edfabecbbceee999169a74d04e1fe4
André Macena <andremacena97@gmail.com>
2026-05-05 12:46:53 -0300

    feat: create new UseCase and a service to hash password

---
+++ src/VctFantasy.Domain/Services/PasswordHasherService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using VctFantasy.Domain.Models;
+
+namespace VctFantasy.Domain.Services
+{
+    public class PasswordHasherService
+    {
+        public string GenerateHash(string password, string salt)
+        {
+            var hash = Rfc2898DeriveBytes.Pbkdf2(Encoding.UTF8.GetBytes(password),
+                            Encoding.UTF8.GetBytes(salt),
+                            350000, // Iterations
+                            HashAlgorithmName.SHA512,  // Algorithm
+                            64); // keysize
+
+            var hashedStr = Convert.ToBase64String(hash);
+
+            return hashedStr;
+        }
+
+        public string GenerateSalt()
+        {
+            var rng = RandomNumberGenerator.Create();
+
+            byte[] salt = new byte[64];
+
+            rng.GetBytes(salt);
+
+            string cryptSalt = Convert.ToBase64String(salt);
+
+            return cryptSalt;
+        }
+    }
+}
---
+++ src/VctFantasy.Domain/UseCases/AuthenticationUseCase.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using VctFantasy.Domain.Context;
+using VctFantasy.Domain.Models;
+
+namespace VctFantasy.Domain.UseCases
+{
+    public class AuthenticationUseCase
+    {
+        private readonly VctFantasyContext _context;
+        public AuthenticationUseCase(VctFantasyContext vctFantasyContext)
+        {
+            _context = vctFantasyContext;
+        }
+
+        public async Task<string> Login(User user)
+        {
+            var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+
+            if (existingUser == null)
+            {
+                return "You are not registered, please register yourself";
+            }
+
+            var passwordHash = Rfc2898DeriveBytes.Pbkdf2(user.PasswordHash,
+                                Encoding.UTF8.GetBytes(existingUser.PasswordSalt),
+                                350000, // Iterations
+                                HashAlgorithmName.SHA512,  // Algorithm
+                                64); // keysize
+
+            bool compareResult = CryptographicOperations.FixedTimeEquals(passwordHash, Convert.FromBase64String(existingUser.PasswordHash));
+
+            if (compareResult)
+            {
+                return "Login Successful";
+            }
+            return "Login not implemented";
+        }
+    }
+}
