bb7997425e8bc8182f87914d6b9168041f3be93b
André Macena <andremacena97@gmail.com>
2026-05-06 23:06:43 -0300

    feat: add dto models

---
+++ src/VctFantasy.Domain/Dtos/TeamDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VctFantasy.Domain.Dtos
+{
+    public class TeamDto
+    {
+
+        public string Name { get; set; }
+        public string PathLogoTeam { get; set; }
+    }
+}
---
+++ src/VctFantasy.Domain/Dtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VctFantasy.Domain.Dtos
+{
+    public class UserDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
