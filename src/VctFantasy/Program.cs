8322f849b8c8386a25ad53be2b68c0492bace159
André Macena <andremacena97@gmail.com>
2026-05-06 23:08:42 -0300

    feat: add new DI

--- src/VctFantasy/Program.cs
+++ src/VctFantasy/Program.cs
@@ -13,7 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddTransient<TokenService>();
 builder.Services.AddTransient<PasswordHasherService>();
+
 builder.Services.AddTransient<RegisterUserUseCase>();
+builder.Services.AddTransient<RegisterTeamUseCase>();
 builder.Services.AddTransient<AuthenticationUseCase>();
 
 builder.Services.AddControllers();
