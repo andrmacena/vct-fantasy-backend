aefd32a8a30fc9dd34e3f538e0bec6a4f0141714
André Macena <andremacena97@gmail.com>
2026-05-16 18:32:04 -0300

    feat: new use cases

---
+++ src/VctFantasy.Domain/UseCases/OrganizationUseCase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VctFantasy.Domain.Context;
+using VctFantasy.Domain.Dtos;
+using VctFantasy.Domain.Models;
+
+namespace VctFantasy.Domain.UseCases
+{
+    public class OrganizationUseCase
+    {
+        private readonly VctFantasyContext _context;
+        public OrganizationUseCase(VctFantasyContext context)
+        {
+            _context = context;
+        }
+
+        public string RegisterOrganization(List<OrganizationDto> organization)
+        {
+            try
+            {
+                foreach (var org in organization)
+                {
+                    var organizationEntity = new Organization
+                    {
+                        Name = org.Name,
+                        Abbreviation = org.Abbreviation,
+                        PathLogoOrg = org.PathLogoOrg
+                    };
+
+                    _context.Organizations.Add(organizationEntity);
+                }
+                _context.SaveChanges();
+
+                return "Organizações registradas com sucesso!";
+            }
+            catch (Exception ex)
+            {
+                
+                return "An error occurred while registering organizations: " + ex.Message;
+            }
+        }
+    }
+}
---
+++ src/VctFantasy.Domain/UseCases/PlayerUseCase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VctFantasy.Domain.Context;
+using VctFantasy.Domain.Dtos;
+using VctFantasy.Domain.Models;
+
+namespace VctFantasy.Domain.UseCases
+{
+    public class PlayerUseCase
+    {
+        private readonly VctFantasyContext _context;
+        public PlayerUseCase(VctFantasyContext context)
+        {
+            _context = context;
+        }
+
+        public string RegisterPlayer(List<PlayerDto> players)
+        {
+            try
+            {
+                foreach (var player in players)
+                {
+                    var playerEntity = new Player
+                    {
+                        Nickname = player.Nickname,
+                        OrganizationId = player.OrganizationId,
+                        PathProfile = player.PathProfile,
+                    };
+                    _context.Players.Add(playerEntity);
+                }
+
+                _context.SaveChanges();
+
+                return "Player registered successfully";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+
+    }
+}
