5729acb1e331fd4d69de1ddc6f06cc00d7c0a71c
André Macena <andremacena97@gmail.com>
2026-05-16 18:31:53 -0300

    feat: new dtos

---
+++ src/VctFantasy.Domain/Dtos/OrganizationDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VctFantasy.Domain.Dtos
+{
+    public class OrganizationDto
+    {
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+        public string PathLogoOrg { get; set; }
+    }
+}
---
+++ src/VctFantasy.Domain/Dtos/PlayerDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VctFantasy.Domain.Dtos
+{
+    public class PlayerDto
+    {
+        public string Nickname { get; set; }
+        public int OrganizationId { get; set; }
+        public string PathProfile { get; set; }
+    }
+}
