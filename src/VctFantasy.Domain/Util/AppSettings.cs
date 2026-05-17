970d7187ed9f4c961ce5db6fb4659f4a1f7f9f81
André Macena <andremacena97@gmail.com>
2026-05-17 15:13:59 -0300

    feat: add new class to bind appsettings

---
+++ src/VctFantasy.Domain/Util/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VctFantasy.Domain.Util
+{
+    public class AppSettings
+    {
+        public string DefaultConnection { get; set; }
+        public string SecretKey { get; set; }
+    }
+}
