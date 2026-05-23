bc1e510d82c2f41297c720e3059b40bab1eff92a
André Macena <andremacena97@gmail.com>
2026-05-23 17:49:20 -0300

    refactor: using namespace dtos

--- src/VctFantasy.Domain/Models/Role.cs
+++ src/VctFantasy.Domain/Models/Role.cs
@@ -14,7 +14,6 @@ namespace VctFantasy.Domain.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-
         public List<User>? Users { get; set; }
     }
 }
