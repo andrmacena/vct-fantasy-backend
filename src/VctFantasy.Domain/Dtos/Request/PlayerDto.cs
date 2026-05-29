3d29474f9f1aa934b99fa90be5db65aea603b687
André Macena <andremacena97@gmail.com>
2026-05-28 21:32:44 -0300

    feat: add update method to player

--- src/VctFantasy.Domain/Dtos/Request/PlayerDto.cs
+++ src/VctFantasy.Domain/Dtos/Request/PlayerDto.cs
@@ -6,8 +6,17 @@ namespace VctFantasy.Domain.Dtos.Request
 {
     public class PlayerDto
     {
-        public string Nickname { get; set; }
-        public int OrganizationId { get; set; }
-        public string PathProfile { get; set; }
+        public string? Nickname { get; set; }
+        public int? OrganizationId { get; set; }
+        public string? PathProfile { get; set; }
+        public decimal Rating { get; set; } = 0;
+        public int Acs { get; set; } = 0;
+        public int Kills { get; set; } = 0;
+        public int Deaths { get; set; } = 0;
+        public int Assists { get; set; } = 0;
+        public int Kast { get; set; } = 0;
+        public int Adr { get; set; } = 0;
+        public int Fb { get; set; } = 0;
+        public int Fd { get; set; } = 0;
     }
 }
