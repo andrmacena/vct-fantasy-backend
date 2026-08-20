f230d18628dcb34afb07ca2013793ab8e0fba899
André Macena <andremacena97@gmail.com>
2026-08-20 11:05:18 -0300

    feat: new controller to get leaderboard

---
+++ src/VctFantasy/Controllers/LeaderboardController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VctFantasy.Domain.Interfaces;
+
+namespace VctFantasy.Controllers
+{
+    [Route("v1/leaderboard")]
+    public class LeaderboardController : Controller
+    {
+        private readonly ILeaderboard _registerLeaderboardUseCase;
+        public LeaderboardController(ILeaderboard registerLeaderboardUseCase)
+        {
+            _registerLeaderboardUseCase = registerLeaderboardUseCase;
+        }
+
+        [HttpGet]
+        public IActionResult Leaderboard()
+        {
+            var retorno = _registerLeaderboardUseCase.GeneralLeaderboard();
+
+            return Ok(new { data = retorno });
+        }
+    }
+}
